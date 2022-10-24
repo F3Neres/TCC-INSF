a316d651da22f1d6015e9ef82b5b4f11d309c145
Milena Custodio de Senna <milena.custodio.senna@gmail.com>
2022-10-24 11:40:14 -0300

    commit aula

--- site/src/pages/home/index.js
+++ site/src/pages/home/index.js
@@ -1,6 +1,15 @@
 import './index.scss';
 import '../common/index.scss'
 import { Link } from 'react-router-dom'
+import combo1 from '../../images/img-combo1.png'
+import combo2 from '../../images/img-combo2.png'
+import combo3 from '../../images/img-combo3.png'
+import combo4 from '../../images/img-combo4.png'
+import cuidados1 from '../../images/img-cuidadosexternos.png'
+import cuidados2 from '../../images/img-cuidadosextras.png'
+
+import cuidados3 from '../../images/img-cuidadosinternos.png'
+
 
 // https://wallpaperscraft.com/catalog/cars
 
@@ -47,29 +56,54 @@ export default function Index() {
             </section>
  
             <section className='faixa-4'>
-                <div className='textos'>
-                    <p className='P2'>Nós oferecemos o melhor serviço para manter o seu carro sempre pronto para um bom passeio ou um dia de trabalho </p>
-                    <p className='P3'>Entre em contato conosco e venha nos conhecer!</p>
-                 </div>
-                 <div className='BOTAO'>   
-                    <div>
-                        <button className='b2'>contato</button>
-                    </div>
-            </div>    
-            </section>
 
-            <section className='faixa-5'>
                 <div className='textos'>
                     <p className='P2'>Nós oferecemos o melhor serviço para manter o seu carro sempre pronto para um bom passeio ou um dia de trabalho </p>
                     <p className='P3'>Entre em contato conosco e venha nos conhecer!</p>
-                 </div>
-                 <div className='BOTAO'>   
-                    <div>
-                        <button className='b2'>contato</button>
+                </div>
+
+                <div className='BOTAO'>   
+                        <button className='b2'>contato</button>                
+                </div>    
+            
+
+                 </section>  
+
+
+                 <section className='faixa-5'>
+                    
+                    <div className='titulo'>
+                    
+                        <h1 className='t1'>NOSSOS COMBOS</h1>
                     </div>
-            </div>    
-            </section>
+
+                    <div className='imagens1'>
+                        <img src={combo1} alt="imagem" width="200em" height="200em" />
+                        <img src={combo2} alt="imagem" width="200em" height="200em" />
+                        <img src={combo3} alt="imagem" width="200em" height="200em" />
+                        <img src={combo4} alt="imagem" width="200em" height="200em" />
+                    </div>
+                    <hr />
+                    
+                    <div className='titulo2'>
+                        <h1 className='t2'>NOSSOS SERVIÇOS ESPECIALIZADOS</h1>
+                        <p className='p5'>SAIBA UM POUCO MAIS SOBRE O QUE FAZEMOS AQUI</p>
+                     
+                    </div>
+
+                    <div className=''></div>
+          
+
                 
+
+               </section>
+
+
+
+
+              
+ 
+
          
             
         </main>
